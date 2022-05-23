23f84ffea4682f976984cb02cba99392d12c5182
anglsftri <angelsafitri416@gmail.com>
2022-05-23 14:06:06 +0700

    SOLID-Srp

---
+++ persegi.py
@@ -0,0 +1,10 @@
+class Persegi:
+    
+    def __init__(self, sisi:float):
+        self.__sisi = sisi
+        
+    def set_sisi(self, sisi: float) -> None:
+        self.__sisi = __sisi
+        
+    def get_sisi(self)-> dloat:
+        return self.__sisi
